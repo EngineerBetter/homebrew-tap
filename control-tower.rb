fcd1a8d69e7af227090b901294a1be0881625744
CI <systems@engineerbetter.com>
2020-12-18 06:32:19 +0000

    Release control-tower version 0.13.12

--- control-tower.rb
+++ control-tower.rb
@@ -6,10 +6,10 @@ class ControlTower < Formula
 
   if OS.mac?
     url "https://github.com/EngineerBetter/control-tower/releases/download/#{version}/control-tower-darwin-amd64"
-    sha256 "893b42792c76be15fef42c4286a81e83f1edcd14a16f02c18c335fd09e9a0c6d"
+    sha256 "d5f217da39f8d4760e7a4481f404f70e415923cc231e9e8c98c9db082ac80fe6"
   elsif OS.linux?
     url "https://github.com/EngineerBetter/control-tower/releases/download/#{version}/control-tower-linux-amd64"
-    sha256 "11ec1434520decefc160a7161e4e220c268359764a3e3d233aab18e0bd826f69"
+    sha256 "1d816aee43f982a0b088496d9d33f54209034259855649c6431a5667d8ad4f7f"
   end
 
   depends_on :arch => :x86_64
