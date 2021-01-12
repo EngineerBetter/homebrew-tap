6696c18b868a0d93d5c0672215aec1a3472bc247
CI <systems@engineerbetter.com>
2021-01-12 20:47:18 +0000

    Release control-tower version 0.14.1

--- control-tower.rb
+++ control-tower.rb
@@ -2,14 +2,14 @@ class ControlTower < Formula
   desc "Deploy and operate Concourse CI in a single command"
   homepage "https://www.engineerbetter.com"
   license "Apache-2.0"
-  version "0.14.0"
+  version "0.14.1"
 
   if OS.mac?
     url "https://github.com/EngineerBetter/control-tower/releases/download/#{version}/control-tower-darwin-amd64"
-    sha256 "78f56e81d4890f466909a896737015090d3e3d81d960543184f18297925c77dd"
+    sha256 "6c5d07a5d113c78f23b4cea5cfae954367ebddd461498131089fb4ada91d1fb7"
   elsif OS.linux?
     url "https://github.com/EngineerBetter/control-tower/releases/download/#{version}/control-tower-linux-amd64"
-    sha256 "acccbb9df57fc72765cfd788b1abee43751991664f3ba66bdf2bd8e902f76907"
+    sha256 "a18e53bd228cfeb5603d7968ae89da3480865f19f7f64f784e31cafc303780a9"
   end
 
   depends_on :arch => :x86_64
