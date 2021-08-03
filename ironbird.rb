986697e6a99a80e8786e4ab485b28ef7ee496593
CI <systems@engineerbetter.com>
2021-08-03 14:31:42 +0000

    Update Ironbird version

--- ironbird.rb
+++ ironbird.rb
@@ -2,14 +2,14 @@ class Ironbird < Formula
   desc "Test Concourse tasks using a YAML DSL for Ginkgo/Gomega"
   homepage "https://www.engineerbetter.com"
   license "Apache-2.0"
-  version "0.0.13"
+  version "0.0.14"
 
   if OS.mac?
     url "https://github.com/EngineerBetter/ironbird/releases/download/#{version}/ironbird-darwin"
-    sha256 "067ba41891b27d9f05e1da0cfd97a03b8fa9b7a9bd3eed157f5e92e3baebeecb"
+    sha256 "a59a915b00db6300c00d765997edd65989d878b71d64cde026d9fd54c9fdade2"
   elsif OS.linux?
     url "https://github.com/EngineerBetter/ironbird/releases/download/#{version}/ironbird-linux"
-    sha256 "db7c8acb51667a7ced57b0dc8f651e64289992076f0e5157cbb8fbd7ebe5c39a"
+    sha256 "46ccd578d1aeca66421a519c2d237af00a00665f4fc10a41abd2c05507d9d17e"
   end
 
   depends_on :arch => :x86_64
