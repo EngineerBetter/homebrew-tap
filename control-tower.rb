97052407e3f2d81644951b280486dc119702262b
CI <systems@engineerbetter.com>
2022-06-17 11:45:09 +0000

    Release control-tower version 0.20.4

--- control-tower.rb
+++ control-tower.rb
@@ -1,28 +1,38 @@
 class ControlTower < Formula
   desc "Deploy and operate Concourse CI in a single command"
   homepage "https://www.engineerbetter.com"
+  version "0.20.4"
   license "Apache-2.0"
-  version "${version}"
+
+  is_arm64 = RUBY_PLATFORM.include("arm64")
 
   if OS.mac?
-    url "https://github.com/EngineerBetter/control-tower/releases/download/#{version}/control-tower-darwin-amd64"
-    sha256 "${darwin_cli_sha256}"
+    if is_arm64
+      url "https://github.com/EngineerBetter/control-tower/releases/download/#{version}/control-tower-darwin-arm64"
+      sha256 "66555504abfc9f7baa331eb53823732716f14cbc8c18c7f4efe738e9ab9e22a2"
+    else
+      url "https://github.com/EngineerBetter/control-tower/releases/download/#{version}/control-tower-darwin-amd64"
+      sha256 "647bc35361db93f9b94e14a15d84a8a9ba0559b53f66fa11a3664f87c111da24"
+    end
   elsif OS.linux?
     url "https://github.com/EngineerBetter/control-tower/releases/download/#{version}/control-tower-linux-amd64"
-    sha256 "${linux_cli_sha256}"
+    sha256 "ebae4302cba7dff7c0ebfd0dc115b81aef1a45f1010468907f7ca4b2fb145bb4"
   end
-
 
   def install
     binary_name = "control-tower"
     if OS.mac?
-      bin.install "control-tower-darwin-amd64" => binary_name
+      if is_arm64
+        bin.install "control-tower-darwin-arm64" => binary_name
+      else
+        bin.install "control-tower-darwin-amd64" => binary_name
+      end
     elsif OS.linux?
       bin.install "control-tower-linux-amd64" => binary_name
     end
   end
 
   test do
-    system "#{bin}/control-tower --help"
-  endc
+    system "#{bin}/control-tower", "--help"
+  end
 end
