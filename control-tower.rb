c07e69c41b77b5a074b3623fcb8e66580aba36e9
CI <systems@engineerbetter.com>
2022-11-29 10:30:02 +0000

    Release control-tower version 0.29.0

--- control-tower.rb
+++ control-tower.rb
@@ -1,20 +1,20 @@
 class ControlTower < Formula
   desc "Deploy and operate Concourse CI in a single command"
   homepage "https://www.engineerbetter.com"
-  version "0.28.0"
+  version "0.29.0"
   license "Apache-2.0"
 
   if OS.mac?
     if RUBY_PLATFORM.include?("arm64")
       url "https://github.com/EngineerBetter/control-tower/releases/download/#{version}/control-tower-darwin-arm64"
-      sha256 "2d61fb310fe86cc42ce55033cb856949356df21dd91b0439804534d75f992014"
+      sha256 "146416d3a580af294191f8ae41922d8dd75e9bd3912ec4188b06abe5a03c655b"
     else
       url "https://github.com/EngineerBetter/control-tower/releases/download/#{version}/control-tower-darwin-amd64"
-      sha256 "0396a3ee073222766f3657390d6b355c241a56990d9e14c6339eab78ae107f6b"
+      sha256 "a11e1750fb85a05b0449bc1d65652427cb1d067d04cc88be9173374f9af4dffe"
     end
   elsif OS.linux?
     url "https://github.com/EngineerBetter/control-tower/releases/download/#{version}/control-tower-linux-amd64"
-    sha256 "c799974ccb829111e58c0fc3c46dc667216715436f146422e9e60bcdf5a17e52"
+    sha256 "c0e77ec3dfd797aaa25b32f2b15e2744c660ce8e791f6fe83990362a4bb85e2c"
   end
 
   def install
